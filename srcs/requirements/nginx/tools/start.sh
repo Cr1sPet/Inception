d83cdec50d9c92626ff810d424efbbadd3f6e3cc
CrsPet <serezha.petrov.02@gmail.com>
2022-09-12 19:51:35 +0300

    working

---
+++ srcs/requirements/nginx/tools/start.sh
@@ -0,0 +1,9 @@
+
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+		-keyout /etc/ssl/private/nginx.key \
+		-out /etc/ssl/certs/nginx.crt \
+		-subj "/C=RU/ST=Tatarstan/L=Kazan/O=wordpress/CN=jchopped.42.fr";
+fi
+
+nginx -g 'daemon off;';
--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -7,24 +7,21 @@ define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-define( 'WP_ALLOW_REPAIR', true );
+define('AUTH_KEY',         'E+qzC>ZoP9Wkx bn.4/tlj)Bw%o`/*|W`vWr%@w7TF?%qx zYDP<x|@W+yr:;~S_');
+define('SECURE_AUTH_KEY',  'Z-X>bki7pxW-l@L-t%bMzsGl@IYiM*snAf-PMR{EnwY[j?Pi|%IuDAV)+s=N:H~C');
+define('LOGGED_IN_KEY',    '(#VoxhH?=1s)_-Vh%MSnkN|r0_Tw]P@LwrglG |jlG~X$Z^f6M@eNx{;x<B7-P|#');
+define('NONCE_KEY',        '{+X.yRW`Rixe+<I6R|wf+,s>P2|6X%6I.1]55wmn#-%:Q05Ekp[c=K+2y^xl)f=2');
+define('AUTH_SALT',        'tD]>>[,KjyNo9[kNpVZI#%@#}=`8$zy $X_={Z^%6i|&Y]nmb(/6gi@5{46Q)2`-');
+define('SECURE_AUTH_SALT', '55;6OND[.l+_wM,de.(+~9?txSdd>W:#jTKl1}<B+]!|x=`/*|y.[ +i|swg|6t{');
+define('LOGGED_IN_SALT',   '%!Z/LnUa{DA5VWz e5$5_|f|KwKc=`-|BlIaFFsLRDn-Y#54*OCO34$+Ub[.v%22');
+define('NONCE_SALT',       'dk|Y-(`WV~GiN+JHZ~z_S/xs^zR~N_c+eXcCww} !LKiP!ZDbbOT5M:,Hl>1N;``');
 
-define('AUTH_KEY',         '+|vAncwR5})|@WR=aN>@vTA0eVO5v=?!r]Z7;qaaN=jm7iAy8%STIJ0Od8aa?*t.');
-define('SECURE_AUTH_KEY',  'vP->hdS6{vCR/> FiQfx-+C)9Fx&r`UO$ -hRMv4,|!|I1_8|6~zj(t-o`1SxFDa');
-define('LOGGED_IN_KEY',    'B9f4jOJ/|re[|Rowu7crYcr`/2I<wNIL>jS]}%cqa[U/g6~-,/7@wSl<v6V;ZpVs');
-define('NONCE_KEY',        'wZ:7jxNjv8 P&!BZ||xe-Nb{p0Unx%,|A+S,Bf>*}t*zFEw`W~0P;}r@~BR`vPCK');
-define('AUTH_SALT',        '1{:!O>BIM6gcGTC-v>%pvp&qm!I?Zb^uF;@kQ<KSP~{ 8Jrn)H-[F=GcYIvZ`a[=');
-define('SECURE_AUTH_SALT', 'T^R-_3G(@dNid$FvZNT*D(-y#jx?x6- s~VUM?QKC$%EeD|<C7/qABa8+;5KX7sX');
-define('LOGGED_IN_SALT',   'xn(Ro.1~0v!!8`#;C?n%PDQ7sD59HE{0PF?h]n$-8ot5 j@W^G&=;lnJRGO_DI5y');
-define('NONCE_SALT',       'wzh {tRvmN/r%TK3zmsz]:r&Nm3K{slZ55&C~A5+J.V(Z|g+/m[K4sqC ] S%YmU');
 $table_prefix = 'wp_';
 
-
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
 require_once ABSPATH . 'wp-settings.php';
-?>
